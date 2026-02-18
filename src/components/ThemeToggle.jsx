084cc5387ed0d5be79013d08b0fde0b7de9ff3c6
MehdiBouchachi <mehdibch.dev@gmail.com>
2026-02-18 19:33:05 +0100

    updating

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { FiDroplet, FiGrid } from "react-icons/fi";
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
+  const [fading, setFading] = useState(false);
+
+  const handleToggle = () => {
+    setFading(true);
+
+    setTimeout(() => {
+      toggleTheme();
+      setFading(false);
+    }, 200);
+    document.documentElement.classList.add("theme-transition");
+
+    setTimeout(() => {
+      document.documentElement.classList.remove("theme-transition");
+    }, 400);
+  };
 
   const Icon = theme === "dark" ? FiDroplet : FiGrid;
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="theme-toggle"
-      aria-label="Toggle theme"
-    >
-      <Icon className="icon" />
-    </button>
+    <>
+      <button
+        onClick={handleToggle}
+        className="theme-toggle"
+        aria-label="Toggle theme"
+      >
+        <Icon className="icon" />
+      </button>
+    </>
   );
 }
